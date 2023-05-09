e8601717898db0f88c2b0a3d8edf269adb171599
Kelvyn Medellin <medellinkelvyn98@gmail.com>
2023-05-09 13:36:03 -0500

    exercises

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,19 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        if (req.getParameter("name") != null) {
+            res.getWriter().println("<h1>Hello, " + req.getParameter("name") + "!</h1>");
+            return;
+        }
+        res.getWriter().println("<h1>Hello, World!</h1>");
     }
 }
